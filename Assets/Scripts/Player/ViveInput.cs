303c0c9a34211dda8b33635ac408ec5e752e8e19
Bill Carolin <billcarolin@users.noreply.github.com>
2019-02-28 22:05:29 -0500

    initial creation of 2.0 vive inputs

---
+++ Assets/Scripts/Player/ViveInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour
+{
+    public SteamVR_Action_Single squeezeAction;
+    public SteamVR_Action_Vector2 touchpadAction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Input._default.inActions.Teleport.GetStateUp(SteamVR_Input_Sources.Any))
+        {
+            Debug.Log("Touchpad Click Action ~~~~~~~~~~~~~~~~~~~~~~>>>");
+        }
+
+        if (SteamVR_Input._default.inActions.Squeeze.GetAxis(SteamVR_Input_Sources.Any) >= 0.02f)
+        {
+            Debug.Log("<<<<<<<<<<<<<<<<<<< Trigger Squeezed Action ~~~~~>>>");
+        }
+
+    }
+}
